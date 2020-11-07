fc407259cc4e2f3bf1311bec33c42f855a87118c
OsmaroB <osma204bonillamestizo@hotmail.com>
2020-11-06 18:39:29 -0600

    Avance sectorizado

--- app/controllers/js/charts.js
+++ app/controllers/js/charts.js
@@ -17,43 +17,13 @@
         // google.charts.setOnLoadCallback(percentRealice);
         google.charts.setOnLoadCallback(Cronograma);
 
-    function Cronograma() {
-
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Task ID');
-      data.addColumn('string', 'Task Name');
-      data.addColumn('string', 'Resource');
-      data.addColumn('date', 'Start Date');
-      data.addColumn('date', 'End Date');
-      data.addColumn('number', 'Duration');
-      data.addColumn('number', 'Percent Complete');
-      data.addColumn('string', 'Dependencies');
-
-      data.addRows([
-        ['Primera semana', 'Semana 1', 'spring',
-         new Date(2020, 9, 14), new Date(2020, 9, 18), null, 100, null],
-         ['Semana 2', 'Semana 2', 'spring',
-         new Date(2020, 9, 21), new Date(2020, 9, 25), null, 100, null],
-        
-      ]);
-
-      var options = {
-        width: 1000,
-        height: 400,
-        gantt: {
-          trackHeight: 30
-        }
-      };
-
-      var chart = new google.visualization.Gantt(document.getElementById('cronograma'));
-      chart.draw(data, options);
-    }
+    
 
 function percentRealice() {
     var data = google.visualization.arrayToDataTable([
     ['Effort', 'Amount given'],
-    ['Restante',     48],
-    ['Avance',     52]
+    ['Restante',     38],
+    ['Avance',     62]
     ]);
 
     var options = {
@@ -62,7 +32,7 @@ function percentRealice() {
         color: 'white',
     },
     legend: 'none',
-    width: 500,
+    width: 400,
     height: 400,
     slices: {
         1: { color: '#175f92' },
@@ -86,7 +56,7 @@ function drawChart1() {
 
     var options = {
     pieHole: 0.4,
-    width: 470,
+    width: 400,
     height: 400
     };
 
